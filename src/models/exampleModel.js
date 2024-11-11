36b7fa8324f0c7732c2d99deb1857b80812d58d1
Ismael Mesa <ismaelmesazyout@gmail.com>
2024-11-11 19:15:34 +0100

    Prueba de tabla de base de datos

--- src/models/exampleModel.js
+++ src/models/exampleModel.js
@@ -17,7 +17,22 @@ const exampleSchema = new mongoose.Schema({
   },
 });
 
+const itineraryAnalyticSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'], // Validation: Name is required
+    minlength: [3, 'Name must be at least 3 characters long'], // Validation: Minimum length
+    maxlength: [50, 'Name must be at most 50 characters long'], // Validation: Maximum length
+  },
+  value: {
+    type: Number,
+    required: [true, 'Value is required'], // Validation: Value is required
+    min: [0, 'Value must be a positive number'], // Validation: Minimum value
+  },
+});
 // Create the model from the schema
 const ExampleModel = mongoose.model('Example', exampleSchema);
 
-export default ExampleModel; // Export the model
+const ItineraryAnalyticModel = mongoose.model('itineraryAnalytic', itineraryAnalyticSchema);
+
+export default { ExampleModel, ItineraryAnalyticModel }; // Export the model
