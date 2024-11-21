095533fc8622346a9ff09cd62be8d33b573e5d4b
Ismael Mesa <ismaelmesazyout@gmail.com>
2024-11-21 15:40:58 +0100

    feat: añadidos archivos para mejorar los errores en las respuestas

--- src/routes/exampleRoute.js
+++ src/routes/exampleRoute.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import * as exampleController from '../controllers/exampleController.js';
 import { validateExample } from '../middlewares/exampleValidator.js';
-import { middlewareMethod } from '../middlewares/dummyMiddelware.js';
 const router = express.Router();
 
 // Define routes
 router.get('/v1/examples', exampleController.getAllExamples);
-router.post('/v1/examples',middlewareMethod,validateExample, exampleController.createExample);
+router.post('/v1/examples',validateExample, exampleController.createExample);
 router.get('/v1/examples/:id', exampleController.getExampleById);
 router.put(
   '/v1/examples/:id',
